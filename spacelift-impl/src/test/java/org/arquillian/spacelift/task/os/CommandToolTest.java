9b2f9ef02a6b6b2ac600c66a3acc5d9bbefda99d
Karel Piwko <kpiwko@redhat.com>
2015-02-01 13:18:07 +0100

    Added test for #25, fixed in previous commit

--- spacelift-impl/src/test/java/org/arquillian/spacelift/task/os/CommandToolTest.java
+++ spacelift-impl/src/test/java/org/arquillian/spacelift/task/os/CommandToolTest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.arquillian.spacelift.Spacelift;
@@ -11,7 +13,6 @@ import org.arquillian.spacelift.execution.Execution;
 import org.arquillian.spacelift.execution.ExecutionException;
 import org.arquillian.spacelift.process.CommandBuilder;
 import org.arquillian.spacelift.process.ProcessResult;
-import org.arquillian.spacelift.task.os.CommandTool;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Rule;
@@ -43,10 +44,50 @@ public class CommandToolTest {
         // previous call caused this task to register
         // NOTE, this is not really useful for Java, but rather for dynamic languages
         ((CommandTool) Spacelift.task("CommandTool")).programName("java").parameters("-help")
-        .execute().await();
+            .execute().await();
     }
 
+    @Test
+    public void addSystemEnv() {
+        // run only on linux
+        Assume.assumeThat(SystemUtils.IS_OS_LINUX, is(true));
 
+        Spacelift.task(CommandTool.class).programName("java").parameters("-help")
+            .addEnvironment("FOO", "bar")
+            .execute().await();
+    }
+
+    @Test
+    public void addSystemEnvMap() {
+        // run only on linux
+        Assume.assumeThat(SystemUtils.IS_OS_LINUX, is(true));
+
+        Map<CharSequence, CharSequence> env = new HashMap<CharSequence, CharSequence>();
+
+        env.put("FOO", "bar");
+        env.put("BAR", new CharSequence() {
+            private final String value ="baz";
+
+            @Override
+            public CharSequence subSequence(int start, int end) {
+                return value.subSequence(start, end);
+            }
+
+            @Override
+            public int length() {
+                return value.length();
+            }
+
+            @Override
+            public char charAt(int index) {
+                return value.charAt(index);
+            }
+        });
+
+        Spacelift.task(CommandTool.class).programName("java").parameters("-help")
+            .addEnvironment(env)
+            .execute().await();
+    }
 
     @Test
     public void runJavaCommandInvalid() {
@@ -72,7 +113,7 @@ public class CommandToolTest {
     }
 
     @Test
-    public void splitToParameters() throws Exception{
+    public void splitToParameters() throws Exception {
         CommandBuilder cb = Spacelift.task(CommandTool.class).programName("java").splitToParameters("-foo -bar").commandBuilder;
         Assert.assertThat(cb.build().getNumberOfParameters(), is(2));
     }
